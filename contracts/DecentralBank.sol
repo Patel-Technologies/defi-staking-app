364620513620949df32fd0dac5946af2c79de0c0
Jay Ghevariya <jay.b.ghevariya@gmail.com>
2023-01-09 17:35:00 +0530

    extended functionality of Decentralbank smart con

--- contracts/DecentralBank.sol
+++ contracts/DecentralBank.sol
@@ -1,8 +1,18 @@
 // SPDX-License-Identifier: GPL-3.0
 pragma solidity >=0.5.0;
 
+import "./Reward.sol";
+import "./Tether.sol";
+
 contract DecentralBank {
     string public name = "Decentral Bank";
     address public owner;
-    
+    Reward reward;
+    Tether tether;
+
+    constructor (Reward _rwd,Tether _tether) public {
+        owner = msg.sender;
+        reward = _rwd;
+        tether = _tether;
+    }
 }
