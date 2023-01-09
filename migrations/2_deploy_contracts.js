eef293b6b2e1d621bdec648acaa02b4b3fa7a790
Jay Ghevariya <jay.b.ghevariya@gmail.com>
2023-01-09 17:21:33 +0530

    updated deployedContract.js

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -2,8 +2,14 @@ const Tether = artifacts.require("Tether");
 const RWD = artifacts.require("Reward");
 const DecentralBank = artifacts.require("DecentralBank");
 
-module.exports = async function(deployer) {
+module.exports = async function(deployer, network, accounts) {
     await deployer.deploy(Tether);
+    const tether = await Tether.deployed()
     await deployer.deploy(RWD);
+    const rwd = await RWD.deployed()
     await deployer.deploy(DecentralBank);
+    const decentralBank = await DecentralBank.deployed()
+
+    await rwd.transfer(decentralBank.address, '1000000000000000000000000') // 1 million tokens to DecentralBank
+    await tether.transfer(accounts[1], '100000000000000000000') // 100 tokens to account[1] for testing purposes
 }
