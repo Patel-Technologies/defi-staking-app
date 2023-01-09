47158c850c05243a338417e16354b9dbd3d64615
Jay Ghevariya <jay.b.ghevariya@gmail.com>
2023-01-09 17:35:44 +0530

    changed method accordingly constructor parameter

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -7,7 +7,7 @@ module.exports = async function(deployer, network, accounts) {
     const tether = await Tether.deployed()
     await deployer.deploy(RWD);
     const rwd = await RWD.deployed()
-    await deployer.deploy(DecentralBank);
+    await deployer.deploy(DecentralBank, rwd.address, tether.address);
     const decentralBank = await DecentralBank.deployed()
 
     await rwd.transfer(decentralBank.address, '1000000000000000000000000') // 1 million tokens to DecentralBank
