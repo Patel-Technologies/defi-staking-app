011df191ab0a957a6344f58ca1c48d388daba9db
Jay Ghevariya <jay.b.ghevariya@gmail.com>
2023-01-11 22:41:03 +0530

    added function to stack amount in decentral bank

--- contracts/DecentralBank.sol
+++ contracts/DecentralBank.sol
@@ -10,9 +10,31 @@ contract DecentralBank {
     Reward reward;
     Tether tether;
 
+    address[] public stakers;
+
+    mapping(address => uint256) public stakingBalance;
+    mapping(address => bool) public hasStaked;
+    mapping(address => bool) public isStaking;
+
     constructor (Reward _rwd,Tether _tether) {
         owner = msg.sender;
         reward = _rwd;
         tether = _tether;
     }
+
+    function stakeTokens(uint256 _amount) public {
+        require(_amount > 0, "amount cannot be 0");
+
+        tether.transferFrom(msg.sender, address(this), _amount);
+
+        stakingBalance[msg.sender] = stakingBalance[msg.sender] + _amount;
+
+        if (!hasStaked[msg.sender]) {
+            stakers.push(msg.sender);
+        }
+
+        hasStaked[msg.sender] = true;
+        isStaking[msg.sender] = true;
+    }
+
 }
