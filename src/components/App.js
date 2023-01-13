22c08af49ddc5db44a2983567f10ecbb9c0171c6
Jay Ghevariya <jay.b.ghevariya@gmail.com>
2023-01-13 20:58:27 +0530

    Revert "Revert "appbar moved to new file component""
    This reverts commit ffa1bc064efed0dbf31c1fbe3f67e87303e0d80a.

--- src/components/App.js
+++ src/components/App.js
@@ -1,67 +1,14 @@
 import * as React from 'react';
-import AppBar from '@mui/material/AppBar';
-import Toolbar from '@mui/material/Toolbar';
-import Typography from '@mui/material/Typography';
-import Container from '@mui/material/Container';
 import './App.css';
+import  AppBar from './AppBar';
 
-function ResponsiveAppBar() {
+function App() {
   const [accountAddress, setAccountAddress] = React.useState('');
 
   return (
-    <AppBar position="static" style={{overflow:"hidden"}}>
-      <Container maxWidth="xl">
-        <Toolbar disableGutters>
-          <div className='navbar_flex'>
-            <Typography 
-            variant="h6"
-            noWrap
-            component="a"
-            sx={{
-              mr: 2,
-              fontFamily: 'monospace',
-              fontWeight: 700,
-              letterSpacing: '.1rem',
-              color: 'inherit',
-              textDecoration: 'none',
-            }}
-            class="bi bi-bank bankIcon">
-
-            </Typography>
-            <div className="nav_bar_flex">
-              <Typography
-                variant="h6"
-                noWrap
-                component="a"
-                sx={{
-                  mr: 2,
-                  fontFamily: 'monospace',
-                  fontWeight: 700,
-                  letterSpacing: '.1rem',
-                  color: 'inherit',
-                  textDecoration: 'none',
-                }}
-                className="bank_name"
-              >
-                Decentral Bank (Yield Staking App)
-              </Typography>
-
-              <Typography
-                sx={{
-                  mr: 2,
-                  fontFamily: 'monospace',
-                  color: 'inherit',
-                  textDecoration: 'none',
-                }}
-                className="account_address"
-              >
-                Account Address: {accountAddress}
-              </Typography>
-            </div>      
-          </div>
-        </Toolbar>
-      </Container>
-    </AppBar>
+    <div>
+      <AppBar accountAddress={accountAddress} setAccountAddress={setAccountAddress} />
+    </div>
   );
 }
-export default ResponsiveAppBar;
+export default App;
