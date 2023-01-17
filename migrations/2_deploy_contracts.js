b612ecd95d00dc69a1cf7bc6e2b19e96a5828fe6
Jay Ghevariya <jay.b.ghevariya@gmail.com>
2023-01-17 20:15:46 +0530

    changed wrong parameter

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -7,7 +7,7 @@ module.exports = async function(deployer, network, accounts) {
     const tether = await Tether.deployed()
     await deployer.deploy(RWD);
     const rwd = await RWD.deployed()
-    await deployer.deploy(DecentralBank, rwd.address, tether.address);
+    await deployer.deploy(DecentralBank,tether.address,rwd.address,);
     const decentralBank = await DecentralBank.deployed()
 
     await rwd.transfer(decentralBank.address, '1000000000000000000000000') // 1 million tokens to DecentralBank
